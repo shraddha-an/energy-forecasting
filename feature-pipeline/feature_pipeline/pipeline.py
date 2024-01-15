71a66634b684c4f2752b96cfdda875e09cd7d067
Shraddha Anala <shraddha.anala@outlook.com>
2024-01-15 12:13:35 +0530

    testing again

--- feature-pipeline/feature_pipeline/pipeline.py
+++ feature-pipeline/feature_pipeline/pipeline.py
@@ -38,6 +38,8 @@ def run(
           A dictionary containing metadata of the pipeline.
     """
     print(f"\nCloned this repo from Paul's main ML Ops repo to test GitHub Actions CI/CD steps...\n")
+    print(f"\nDidn't configure GitHub Actions workflow before so testing GitHub Actions CI/CD steps again...\n")
+
     logger.info(f"Extracting data from API.")
     data, metadata = extract.from_file(
         export_end_reference_datetime, days_delay, days_export, url
